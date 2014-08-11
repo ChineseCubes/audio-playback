fe0a1629d4066137acf10f8dd245c74e217400b5
caasi <caasi.igd@gmail.com>
2014-08-11 15:52:26 +0800

    try Buzz

--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
 (function(){
-  var noop, $dropbox;
+  var noop, playWith, $dropbox;
   noop = function(e){
     var x$;
     x$ = e;
     x$.preventDefault();
     x$.stopPropagation();
   };
-  $dropbox = $('#dropbox').on('dragover', noop).on('dragenter', noop).on('drop', function(e){
-    var file, ref$, ref1$, x$, reader;
-    noop(e);
-    file = (ref$ = e.originalEvent.dataTransfer) != null ? (ref1$ = ref$.files) != null ? ref1$[0] : void 8 : void 8;
-    x$ = reader = new FileReader;
-    x$.readAsDataURL(file);
-    x$.onload = function(e){
+  playWith = {
+    audio: function($container, uri, rate){
       var $audio, audio;
-      $audio = $("<audio src=\"" + e.target.result + "\" controls preload=\"auto\" autoplay=\"true\">");
-      $dropbox.html($audio);
+      $audio = $("<audio src=\"" + uri + "\" controls preload=\"auto\" autoplay=\"true\">");
+      $container.html($audio);
       return audio = $audio.get()[0].playbackRate = 0.5;
-    };
-    return x$;
+    },
+    howler: function($container, uri, rate){
+      console.log('howler');
+      return playWith.howler.sound = new Howl({
+        urls: [uri],
+        autoplay: true,
+        rate: rate
+      });
+    },
+    sound: function($container, uri, rate){
+      var x$;
+      x$ = createjs.Sound;
+      x$.registerSound({
+        src: uri,
+        id: 'sound'
+      });
+      x$.addEventListener('fileload', function(e){
+        console.log(e);
+        return createjs.Sound.play('sound');
+      });
+      return x$;
+    },
+    buzz: function($container, uri, rate){
+      var ref$, path, ext, sound;
+      ref$ = uri.split('.'), path = ref$[0], ext = ref$[1];
+      sound = new buzz.sound(path, {
+        formats: [ext],
+        autoplay: true
+      });
+      return sound.setSpeed(rate);
+    }
+  };
+  $dropbox = $('#dropbox').click(function(e){
+    var path;
+    noop(e);
+    path = 'assets/recording.mp3';
+    playWith['buzz']($dropbox, path, 0.5);
+    return console.log('play');
   });
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -3,16 +3,41 @@ noop = !(e) ->
     ..preventDefault!
     ..stopPropagation!
 
+play-with =
+  audio: ($container, uri, rate) ->
+    $audio = $ "<audio src=\"#uri\" controls preload=\"auto\" autoplay=\"true\">"
+    $container.html $audio
+    audio = $audio.get!0
+      .playbackRate = 0.5
+  howler: ($container, uri, rate) ->
+    console.log \howler
+    play-with.howler.sound = new Howl do
+      urls: [uri]
+      autoplay: true
+      rate: rate
+  sound: ($container, uri, rate) ->
+    createjs.Sound
+      ..registerSound do
+        src: uri
+        id: \sound
+      ..addEventListener \fileload (e) ->
+        console.log e
+        createjs.Sound.play \sound
+  buzz: ($container, uri, rate) ->
+    [path, ext] = uri.split '.'
+    sound = new buzz.sound do
+      path
+      formats: [ext]
+      autoplay: on
+    sound.setSpeed rate
+
 $dropbox = $ \#dropbox
-  .on \dragover noop
-  .on \dragenter noop
-  .on \drop (e) ->
+  #.on \dragover noop
+  #.on \dragenter noop
+  #.on \drop (e) ->
+  #  file = e.originalEvent.dataTransfer?files?0
+  .click (e) ->
     noop e
-    file = e.originalEvent.dataTransfer?files?0
-    reader = new FileReader
-      ..readAsDataURL file
-      ..onload = (e) ->
-        $audio = $ "<audio src=\"#{e.target.result}\" controls preload=\"auto\" autoplay=\"true\">"
-        $dropbox.html $audio
-        audio = $audio.get!0
-          .playbackRate = 0.5
+    path = 'assets/recording.mp3'
+    play-with['buzz'] $dropbox, path, 0.5
+    console.log \play
