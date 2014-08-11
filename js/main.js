43c7c958f9d967bf060bb08359aad93c64daa6bd
caasi <caasi.igd@gmail.com>
2014-08-11 12:28:57 +0800

    playbackRate at <audio> is not working in Safari

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 (function(){
-  console.log('foobar');
+  var noop, $dropbox;
+  noop = function(e){
+    var x$;
+    x$ = e;
+    x$.preventDefault();
+    x$.stopPropagation();
+  };
+  $dropbox = $('#dropbox').on('dragover', noop).on('dragenter', noop).on('drop', function(e){
+    var file, ref$, ref1$, x$, reader;
+    noop(e);
+    file = (ref$ = e.originalEvent.dataTransfer) != null ? (ref1$ = ref$.files) != null ? ref1$[0] : void 8 : void 8;
+    x$ = reader = new FileReader;
+    x$.readAsDataURL(file);
+    x$.onload = function(e){
+      var $audio, audio;
+      $audio = $("<audio src=\"" + e.target.result + "\" controls preload=\"auto\" autoplay=\"true\">");
+      $dropbox.html($audio);
+      return audio = $audio.get()[0].playbackRate = 0.5;
+    };
+    return x$;
+  });
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,1 +1,18 @@
-console.log \foobar
+noop = !(e) ->
+  e
+    ..preventDefault!
+    ..stopPropagation!
+
+$dropbox = $ \#dropbox
+  .on \dragover noop
+  .on \dragenter noop
+  .on \drop (e) ->
+    noop e
+    file = e.originalEvent.dataTransfer?files?0
+    reader = new FileReader
+      ..readAsDataURL file
+      ..onload = (e) ->
+        $audio = $ "<audio src=\"#{e.target.result}\" controls preload=\"auto\" autoplay=\"true\">"
+        $dropbox.html $audio
+        audio = $audio.get!0
+          .playbackRate = 0.5
